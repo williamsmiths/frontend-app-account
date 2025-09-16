d77019512eb0180fb0292ca16a024af0d14eb3aa
Thien Mai <63228267+thienmdp@users.noreply.github.com>
2025-09-16 15:36:52 +0700

    feat: update logo & footer

--- src/index.jsx
+++ src/index.jsx
@@ -1,55 +1,53 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
-import 'formdata-polyfill';
-import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
-import {
-  subscribe, initialize, APP_INIT_ERROR, APP_READY, mergeConfig,
-} from '@edx/frontend-platform';
-import React, { StrictMode } from 'react';
+import "formdata-polyfill";
+import { AppProvider, ErrorPage } from "@edx/frontend-platform/react";
+import { subscribe, initialize, APP_INIT_ERROR, APP_READY, mergeConfig } from "@edx/frontend-platform";
+import React, { StrictMode } from "react";
 // eslint-disable-next-line import/no-unresolved
-import { createRoot } from 'react-dom/client';
-import { Route, Routes, Outlet } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import { Route, Routes, Outlet } from "react-router-dom";
 
-import Header from '@edx/frontend-component-header';
-import { FooterSlot } from '@edx/frontend-component-footer';
+import Header from "@edx/frontend-component-header";
+import { FooterSlot } from "@edx/frontend-component-footer";
 
-import configureStore from './data/configureStore';
-import AccountSettingsPage, { NotFoundPage } from './account-settings';
-import IdVerificationPageSlot from './plugin-slots/IdVerificationPageSlot';
-import messages from './i18n';
+import configureStore from "./data/configureStore";
+import AccountSettingsPage, { NotFoundPage } from "./account-settings";
+import IdVerificationPageSlot from "./plugin-slots/IdVerificationPageSlot";
+import messages from "./i18n";
 
-import './index.scss';
-import Head from './head/Head';
+import "./index.scss";
+import Head from "./head/Head";
+import CustomFooter from "./components/CustomFooter";
 
-const rootNode = createRoot(document.getElementById('root'));
+const rootNode = createRoot(document.getElementById("root"));
 subscribe(APP_READY, () => {
   rootNode.render(
     <StrictMode>
       <AppProvider store={configureStore()}>
         <Head />
         <Routes>
-          <Route element={(
-            <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
-              <Header />
-              <main className="flex-grow-1" id="main">
-                <Outlet />
-              </main>
-              <FooterSlot />
-            </div>
-        )}
+          <Route
+            element={
+              <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
+                <Header />
+                <main className="flex-grow-1" id="main">
+                  <Outlet />
+                </main>
+                {/* <FooterSlot /> */}
+                <CustomFooter />
+              </div>
+            }
           >
-            <Route
-              path="/id-verification/*"
-              element={<IdVerificationPageSlot />}
-            />
+            <Route path="/id-verification/*" element={<IdVerificationPageSlot />} />
             <Route path="/" element={<AccountSettingsPage />} />
             <Route path="/notfound" element={<NotFoundPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
       </AppProvider>
-    </StrictMode>,
+    </StrictMode>
   );
 });
 
@@ -63,17 +61,22 @@ initialize({
   hydrateAuthenticatedUser: true,
   handlers: {
     config: () => {
-      mergeConfig({
-        SUPPORT_URL: process.env.SUPPORT_URL,
-        SHOW_EMAIL_CHANNEL: process.env.SHOW_EMAIL_CHANNEL || 'false',
-        ENABLE_COPPA_COMPLIANCE: (process.env.ENABLE_COPPA_COMPLIANCE || false),
-        ENABLE_ACCOUNT_DELETION: (process.env.ENABLE_ACCOUNT_DELETION !== 'false'),
-        COUNTRIES_WITH_DELETE_ACCOUNT_DISABLED: JSON.parse(process.env.COUNTRIES_WITH_DELETE_ACCOUNT_DISABLED || '[]'),
-        ENABLE_DOB_UPDATE: (process.env.ENABLE_DOB_UPDATE || false),
-        MARKETING_EMAILS_OPT_IN: (process.env.MARKETING_EMAILS_OPT_IN || false),
-        PASSWORD_RESET_SUPPORT_LINK: process.env.PASSWORD_RESET_SUPPORT_LINK,
-        LEARNER_FEEDBACK_URL: process.env.LEARNER_FEEDBACK_URL,
-      }, 'App loadConfig override handler');
+      mergeConfig(
+        {
+          SUPPORT_URL: process.env.SUPPORT_URL,
+          SHOW_EMAIL_CHANNEL: process.env.SHOW_EMAIL_CHANNEL || "false",
+          ENABLE_COPPA_COMPLIANCE: process.env.ENABLE_COPPA_COMPLIANCE || false,
+          ENABLE_ACCOUNT_DELETION: process.env.ENABLE_ACCOUNT_DELETION !== "false",
+          COUNTRIES_WITH_DELETE_ACCOUNT_DISABLED: JSON.parse(
+            process.env.COUNTRIES_WITH_DELETE_ACCOUNT_DISABLED || "[]"
+          ),
+          ENABLE_DOB_UPDATE: process.env.ENABLE_DOB_UPDATE || false,
+          MARKETING_EMAILS_OPT_IN: process.env.MARKETING_EMAILS_OPT_IN || false,
+          PASSWORD_RESET_SUPPORT_LINK: process.env.PASSWORD_RESET_SUPPORT_LINK,
+          LEARNER_FEEDBACK_URL: process.env.LEARNER_FEEDBACK_URL,
+        },
+        "App loadConfig override handler"
+      );
     },
   },
 });
